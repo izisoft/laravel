5820f1965e760c50f7f62ca873f6c544f6c6c19c
Code dạo kiếm cơm <zinzinx8@gmail.com>
2020-07-21 23:17:26 +0700

    Update ModuleServiceProvider.php

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -12,154 +12,16 @@ class ModuleServiceProvider extends RouteServiceProvider{
 
     protected $_route = [];
     
-    public function register(){}
+    public function register(){
+        // echo __METHOD__. '</br>';
+    }
 
     public function boot(){
-
-    	/**
-         * Define $_server
-         */
-    	if(!empty($info = $this->getServerInfo()))
-        foreach($info as $k=>$v){
-            defined($k) or define($k,$v);
-        }
-
-        if(!(defined('SERVER_PROTOCOL') && !SERVER_PROTOCOL)){
-	        // get shop by domain
-	        $shop = $this->getShopInfo();
-	        $DOMAIN_INVISIBLED = false;
-	        $domain_module = false;
-	        $domain_module_name = '';
-	        if(!empty($shop)){
-	        	define ('SHOP_STATUS',$shop->status);
-	            define ('__SID__',$shop->sid);
-	            define ('DOMAIN_TEMPLATE',$shop->temp_id);
-	            define ('__SITE_NAME__',$shop->code);
-	         //    define ('__TEMPLATE_DOMAIN_STATUS__',$shop['state']);
-	            define ('DOMAIN_LANGUAGE',$shop->lang);
-	         //    define ('DOMAIN_LANGUAGE',!in_array($shop['lang'], ['', 'auto']) ? $shop['lang'] : '');
-
-	            if($shop->module != ""){
-	                $this->_route['module'] = $domain_module_name = $shop->module;
-	                $domain_module = true;
-	                
-	            }
-	            
-	            $DOMAIN_INVISIBLED = isset($shop->is_hidden) && $shop->is_hidden == 1 ? true : false;
-
-	        }else{
-	        	define ('SHOP_STATUS',0);
-	            define ('__SID__',0);
-	            define ('DOMAIN_LAYOUT', 'main');
-	            define ('__TEMPLATE_DOMAIN_STATUS__',1);
-	            define ('SHOP_CATEGORY', 0);
-	        }
-	        //
-
-	        defined('DOMAIN_HIDDEN') or define('DOMAIN_HIDDEN', $DOMAIN_INVISIBLED);
-	        defined('__DOMAIN_MODULE__') or define('__DOMAIN_MODULE__', $domain_module);
-	        defined('__DOMAIN_MODULE_NAME__') or define('__DOMAIN_MODULE_NAME__', $domain_module_name);
-    	}
-        //
+        
         $this->setNamespace();
 
         parent::boot();        
-    }
-
-    /**
-    *	Prepare domain
-    *
-    */
-    /**
-     * Parse $_SERVER
-     * @return string[]|unknown[]|mixed[]|NULL[]
-     */
-    
-    protected function getServerInfo(){
-        $s = $_SERVER;        
-        
-        $ssl = false;
-        
-        if(isset($s['HTTPS']) && $s['HTTPS'] == 'on'){
-            $ssl = true;
-        }elseif(isset($s['HTTP_X_FORWARDED_PROTO']) && strtolower($s['HTTP_X_FORWARDED_PROTO']) == 'https'){
-            $ssl = true;
-        }else{
-            $ssl = isset($s['SERVER_PORT']) && $s['SERVER_PORT'] == 443;
-        }
-        
-         
-
-        $sp = isset($s['SERVER_PROTOCOL']) ? strtolower($s['SERVER_PROTOCOL']) : false;
-
-        if(!$sp) {
-        	define('SERVER_PROTOCOL', false);
-        	return;
-        }
-
-        $protocol = substr($sp, 0, strpos($sp, '/')) . (($ssl) ? 's' : '');
-        
-        $SERVER_PORT = isset($s['SERVER_PORT']) ? $s['SERVER_PORT'] : 80;
-        
-        $port = $SERVER_PORT;
-        $port = in_array($SERVER_PORT , ['80','443']) ? '' : ':'.$port;
-        
-        
-        $host = isset($s['HTTP_X_FORWARDED_HOST']) ? $s['HTTP_X_FORWARDED_HOST'] : (isset($s['HTTP_HOST']) ? $s['HTTP_HOST'] : $s['SERVER_NAME']);
-        $path = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : (isset($_SERVER['HTTP_X_ORIGINAL_URL']) ? $_SERVER['HTTP_X_ORIGINAL_URL'] : $_SERVER['QUERY_STRING']);
-        $url = $protocol . '://' . $host . $port . $path;
-        $pattern = ['/index\.php\//','/index\.php/'];
-        $replacement = ['',''];
-        $url = preg_replace($pattern, $replacement, $url);
-        $a = parse_url($url);
-        $a['host'] = preg_replace('/:\d+$/','', strtolower($a['host']));
-        return [
-            'FULL_URL'=>$url,
-            'URL_NO_PARAM'=> $a['scheme'].'://'.$a['host'].$port.$a['path'],
-            'URL_WITH_PATH'=>$a['scheme'].'://'.$a['host'].$port.$a['path'],
-            'URL_NOT_SCHEME'=>$a['host'].$port.$a['path'],
-            'ABSOLUTE_DOMAIN'=>$a['scheme'].'://'.$a['host'],
-            'URL_QUERY'=>isset($a['query']) ? $a['query'] : '',
-            'DYNAMIC_SCHEME_DOMAIN'  =>  '//'.$a['host'].$port,
-            'SCHEME'=>$a['scheme'],
-            'DOMAIN'=>$a['host'],
-            "__DOMAIN__"=>$a['host'],
-            'DOMAIN_NOT_WWW'=>preg_replace('/www./i','',$a['host'],1),
-            'URL_NON_WWW'=>preg_replace('/www./i','',$a['host'],1),
-            'URL_PORT'=>$port,
-            'URL_PATH'=>$a['path'],
-            '__TIME__'=>time(),
-            'DS' => '/',
-            'ROOT_USER'=>'root',
-            'ADMIN_USER'=>'admin',
-            'DEV_USER'=>'dev',
-            'DEMO_USER'=>'demo',
-            'USER'=>'user'
-        ];
-    }
-    
-    /**
-	* Get shop info from domain
-	*
-    */
-	public function getShopInfo()
-	{
-		return \DB::table('domain_configs as a')
-            ->join('shops as b', 'a.sid', '=', 'b.id')    
-            ->where(['a.domain'=>__DOMAIN__])        
-            ->select([
-                'a.sid',
-                'a.is_hidden',
-                'b.status',
-                'b.code',
-                'a.module',
-                'a.temp_id',              
-                'a.lang',
-                'a.domain',
-            ])
-            ->first();
-	}
-
+    }     
 
     /**
      * Override the map() function of Illuminate\Foundation\Support\Providers\RouteServiceProvider
@@ -172,9 +34,9 @@ class ModuleServiceProvider extends RouteServiceProvider{
 
         $method = "map" . $modules[$this->mapWhat]['folder'];
         if(method_exists($this, $method)){
-        	$this->$method($modules[$this->mapWhat]);
+            $this->$method($modules[$this->mapWhat]);
         }else{
-        	$this->mapModule($modules[$this->mapWhat]);
+            $this->mapModule($modules[$this->mapWhat]);
         }        
     }
 
@@ -192,15 +54,15 @@ class ModuleServiceProvider extends RouteServiceProvider{
         //dd(request()->getRouteResolver());
 
         foreach ($modules as $name => $module) {
-        	if(isset($module['prefix_url']) && $module['prefix_url'] != "")
-        	{
-        		if(request()->is($module['prefix_url']) || request()->is($module['prefix_url'] . '/*')){
-		            $this->namespace = join('\\', ['App', 'Modules', $module['folder'], 'Controllers']);
-		            $this->mapWhat = $name;
-		            $isBreak = true;		            
-		        } 
-        	} 
-        	if($isBreak) break;
+            if(isset($module['prefix_url']) && $module['prefix_url'] != "")
+            {
+                if(request()->is($module['prefix_url']) || request()->is($module['prefix_url'] . '/*')){
+                    $this->namespace = join('\\', ['App', 'Modules', $module['folder'], 'Controllers']);
+                    $this->mapWhat = $name;
+                    $isBreak = true;                    
+                } 
+            } 
+            if($isBreak) break;
         }
          
     }
@@ -223,7 +85,7 @@ class ModuleServiceProvider extends RouteServiceProvider{
         }
         
         Route::middleware($middleware)
-        	->prefix($mod['prefix_url'])
+            ->prefix($mod['prefix_url'])
             ->namespace($this->namespace)
             ->group($route_file);
         
@@ -267,4 +129,4 @@ class ModuleServiceProvider extends RouteServiceProvider{
         }
     }
 
-}
+}   
